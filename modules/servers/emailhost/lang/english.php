db3039e029dec475784a2de169594500d15b27b6
Aman Kumar Tiwari <85321605+theamankr@users.noreply.github.com>
2025-02-06 10:50:31 +0530

    Create english.php

---
+++ modules/servers/emailhost/lang/english.php
@@ -0,0 +1,5 @@
+<?php
+$_LANG['emailhost']['configTitle'] = "Email Hosting Configuration";
+$_LANG['emailhost']['provisionTitle'] = "Provision Email Account";
+$_LANG['emailhost']['loginTitle'] = "Email Login";
+?>
