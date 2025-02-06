6a12a6ffd77e5b3f24c7613e6053325b534d3cae
Aman Kumar Tiwari <85321605+theamankr@users.noreply.github.com>
2025-02-06 10:57:29 +0530

    Update english.php

--- modules/addons/emailhostaddon/lang/english.php
+++ modules/addons/emailhostaddon/lang/english.php
@@ -1,1 +1,5 @@
-
+<?php
+$_ADDONLANG['emailhostaddon']['manageEmails'] = "Manage Emails";
+$_ADDONLANG['emailhostaddon']['setupAutoresponders'] = "Set Up Autoresponders";
+$_ADDONLANG['emailhostaddon']['setupForwarding'] = "Set Up Email Forwarding";
+?>
